ed8ac77b13f882464f66c909fb3f2b527ea08d32
bewe <bewe@bewes-MacBook-Pro.local>
2021-12-08 18:25:30 -0500

    Update APIKEY from google maps to fix error on map

--- src/components/Map.js
+++ src/components/Map.js
@@ -8,10 +8,11 @@ const Map = ({ eventData, center, zoom }) => {
   const [ fireInfo, setFireInfo ] = useState(null);
 
   //Creamos todos los marcadores con la informacion que recibimos
-  const markers = eventData.map((ev) => {
+  const markers = eventData.map((ev, index) => {
     if (ev.categories[0].id === 8) {
       return (
         <LocationMarker
+          key={index}
           lat={ev.geometries[0].coordinates[1]}
           lng={ev.geometries[0].coordinates[0]}
           clicked = {() => (setFireInfo({id: ev.id, title: ev.title}))}
@@ -25,7 +26,7 @@ const Map = ({ eventData, center, zoom }) => {
   return (
     <div className="map">
       <GoogleMapReact
-        bootstrapURLKeys={{ key: "AIzaSyDbfd5YSayqRiSKmxycFjsK0ZsIWfwLoEA" }}
+        bootstrapURLKeys={{ key: "AIzaSyC6oG35-v8dbn7Zb3cvi3EQY7ClaQVTirs" }}
         defaultCenter={center}
         defaultZoom={zoom}
       >
